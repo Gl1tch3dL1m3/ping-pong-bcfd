10b2b3c763580c6bef95a28629e9f8e923760be0
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2024-04-01 18:23:03 +0200

    Create singleplayer.js

---
+++ singleplayer.js
@@ -0,0 +1,21 @@
+$eval
+/*
+
+
+
+*/
+var title = ""
+var desc = ""
+
+var mode$ID = (mode$ID == null) ? 0 : mode$ID
+
+if (mode$ID > 0) {
+  title = "Error!"
+  desc = "You are already playing with someone or have a pending request. You must finish that match before playing with the bot. ❌"
+}
+else {
+  mode$ID = 3
+  title = "Let's play!"
+  desc = "Use `pp!ping` to ping a ball to the bot. The bot pings the ball back to you and this repeats until someone loses. $name starts first. Have fun! 🎲"
+}
+$halt $get(title)
