ed955f70f57ab90254f118aecbb26a2eff61baaa
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2024-04-01 13:32:01 +0200

    Create finish.js

---
+++ finish.js
@@ -0,0 +1,40 @@
+/*
+
+FINISH COMMAND - READ THIS BEFORE INSERTING!
+Open your finish command and...
+
+if you want an embed message, insert this script into "title" field.
+Then type "$get(desc)" in "description" field.
+
+if you want a normal message, insert this script into it, but replace "$get(title)" with "$get(desc)"
+
+*/
+
+$eval
+var title = ""
+var desc = ""
+
+try {
+  if (oppo$mentionedID == $ID) {
+    title = "Match finished!"
+    desc = "Your current match was successfully finished. ✅"
+
+    delete turn$ID
+    delete turn$mentionedID
+    delete oppo$ID
+    delete mode$ID
+    delete isReqCreator$ID
+    delete oppo$mentionedID
+    delete mode$mentionedID
+    delete isReqCreator$mentionedID
+  }
+  else {
+    title = "Error!"
+    desc = "You are not playing with this user. ❌"
+  }
+}
+catch(e) {
+  title = "Error!"
+  desc = "You aren't playing with this user or you didn't mention anyone. ❌"
+}
+$halt $get(title)
