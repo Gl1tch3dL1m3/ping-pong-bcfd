16b0f75d226fdc9b059dd803c91fec6abc4bf446
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2024-04-01 13:37:46 +0200

    Create reaction❌.js

---
+++ reaction❌.js
@@ -0,0 +1,35 @@
+/* 
+
+❌ REACTION EVENT - READ THIS BEFORE INSERTING!
+Open your reaction event and...
+
+if you want an embed message, insert this script into "title" field.
+Then type "$get(desc)" in "description" field.
+
+if you want a normal message, insert this script into it, but replace "$get(title)" with "$get(desc)"
+
+*/
+
+$eval
+/*
+Modes:
+0 - Not playing
+1 - Request active
+2 - Playing
+*/
+
+var title = ""
+var desc = ""
+
+try {
+  if (mode$ID == 1 && isReqCreator$ID == 0) {
+    title = "Request denied!"
+    desc = "<@" + oppo$ID + ">'s request has been denied. The match is cancelled. ❌"
+    delete mode$ID
+    delete oppo$ID
+    delete isReqCreator$ID
+  }
+} catch(e) {
+  // Leave this blank...
+}
+$halt $get(title)
